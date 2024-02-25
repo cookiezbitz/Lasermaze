ac78ef78f842d2d8025ad228cc64bd5b0e098dad
cookiezbitz <remiellin0.0@gmail.com>
2024-02-25 22:37:15 +0800

    added accomodations for lots of lasers

---
+++ Collisions/voltageReadeer/voltageReadeer.ino
@@ -0,0 +1,58 @@
+bool penis;
+float voltage;
+float voltage2;
+float voltage3;
+float voltage4;
+float voltage5;
+int sensorValue;
+int sensorValue2;
+int sensorValue3;
+int sensorValue4;
+int sensorValue5;
+
+void setup() {
+  // start the serial communication
+  Serial.begin(9600);
+  pinMode(13, OUTPUT);
+  
+}
+
+void loop() {
+  // read the input on analog pin A0
+   sensorValue = analogRead(A0);
+   sensorValue2 = analogRead(A1);
+   sensorValue3 = analogRead(A2);
+   sensorValue4 = analogRead(A3);
+   sensorValue5 = analogRead(A4);
+  // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V)
+   voltage = sensorValue * (5.0 / 1023.0);
+   voltage2 = sensorValue2 * (5.0 / 1023.0);
+   voltage3 = sensorValue3 * (5.0 / 1023.0);
+   voltage4 = sensorValue4 * (5.0 / 1023.0);
+   voltage5 = sensorValue5 * (5.0 / 1023.0);
+  // print out the value in volts
+  Serial.print("Voltage at A0: ");
+Serial.println(voltage);
+Serial.print("Voltage at A1: ");
+Serial.println(voltage2);
+  Serial.print("Voltage at A2: ");
+Serial.println(voltage3);
+Serial.print("Voltage at A3: ");
+Serial.println(voltage4);
+Serial.print("Voltage at A4: ");
+Serial.println(voltage5);
+
+
+  if(voltage < 1.85 || voltage2 < 1.85 || voltage3 < 1.85 || voltage4 < 1.85 || voltage5 < 1.85){
+    penis = true;
+}
+Serial.print("Trip Status: ");
+Serial.println(penis);
+
+  if(penis){
+    digitalWrite(13, HIGH);
+  }
+
+  // delay before the next reading
+  delay(1000);
+}
